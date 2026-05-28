21244cafb4f111dcada54296d08e5c82e7cca84d
norbertdmjn <norbertodmjn@outlook.com>
2026-05-28 11:48:32 -0300

    finalizar tabela animais

--- animais/models.py
+++ animais/models.py
@@ -2,15 +2,35 @@ from django.db import models
 
 
 class SexoAnimal(models.Choices):
-    MACHO = "M", "Macho"
-    FEMEA = "F", "Fêmea"
-    INDETERMINADO = "I", "Indeterminado"
+    MACHO = 'M', 'Macho'
+    FEMEA = 'F', 'Fêmea'
+    INDETERMINADO = 'I', 'Indeterminado'
+
+
+class IdadeAproximada(models.Choices):
+    FILHOTE = 'Filhote'
+    ADULTO = 'Adulto'
+    IDOSO = 'Idoso'
+
+
+class Medicamento(models.Choices):
+    SIM = 'Sim'
+    NAO = 'Não'
+    NAO_SABE = 'Nâo sei'
+
+
+class Vacina(models.Choices):
+    SIM = 'Sim'
+    NAO = 'Não'
+    NAO_SABE = 'Nâo sei'
 
 
 class Animais(models.Model):
     nome = models.CharField(50, null=True, blank=True)
     raca = models.CharField(50, null=True, blank=True)
     sexo = models.CharField(max_length=1, choices=SexoAnimal)
-
-    def __str__(self):
-        return f"{self.nome} ({self.get_sexo_display()})"
+    idade = models.CharField(2, null=True, blank=True)
+    idade_aproximada = models.CharField(10, choices=IdadeAproximada)
+    medicamentos = models.CharField(10, choices=Medicamento)
+    vacinacao = models.CharField(10, choices=Vacina)
+    contato = models.CharField(15)
