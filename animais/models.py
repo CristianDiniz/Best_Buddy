3b02bf6ac557c9a2d75d75df3421e9712be5cf16
norbertodmjn <norbertodmjn@outlook.com>
2026-05-27 21:55:44 -0300

    iniciar criação do models do modulo animais

--- animais/models.py
+++ animais/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class SexoAnimal(models.Choices):
+    MACHO = "M", "Macho"
+    FEMEA = "F", "Fêmea"
+    INDETERMINADO = "I", "Indeterminado"
+
+
+class Animais(models.Model):
+    nome = models.CharField(50, null=True, blank=True)
+    raca = models.CharField(50, null=True, blank=True)
+    sexo = models.CharField(max_length=1, choices=SexoAnimal)
+
+    def __str__(self):
+        return f"{self.nome} ({self.get_sexo_display()})"
