58ae0583a15f1274db1fbb794ff81d68c345dae7
CRISTIAN DINIZ <cristianhenrrique706@gmail.com>
2026-05-27 21:48:56 -0300

    alteração do molde Ongs

--- ongs/admin.py
+++ ongs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(pessoa)
+class pessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email', 'telefone')
+    search_fields = ('nome', 'sobrenome', 'email')
--- ongs/models.py
+++ ongs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Ong(models.Model):
+    nome = models.CharField(max_length=100, blank = true, notnull = true)
+    cnpj = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+    update_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return self.nome 
