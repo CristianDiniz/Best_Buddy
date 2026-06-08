4b37db57a3a38a01338795c99c0cd4fbc56a83a5
norbertodmjn <norbertodmjn@outlook.com>
2026-06-08 10:36:38 -0300

    feat: adiciona animais no painel admin

--- animais/admin.py
+++ animais/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Animais
+
 
 # Register your models here.
+@admin.register(Animais)
+class AnimaisAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+    list_display = (
+        'nome',
+        'raca',
+        'sexo',
+        'idade',
+        'idade_aproximada',
+        'medicamentos',
+        'vacinacao',
+        'contato',
+        'created_at',
+    )
+    list_filter = (
+        'raca',
+        'sexo',
+        'idade',
+        'idade_aproximada',
+        'medicamentos',
+        'vacinacao',
+    )
